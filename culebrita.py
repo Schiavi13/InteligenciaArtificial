08503d5c0ea01cc7ada7aa35d5fef0324b368495
Luis Martínez <lufmartinezmu@gmail.com>
2019-04-07 17:09:35 -0500

    Update culebrita.py
    Se corrigen bugs de movilidad, se detecta la colisión con el borde, se detecta la colisión con la comida, elimina la comida y se agrega una parte más al cuerpo (con bugs en la actualización de los sprites de la cola)

--- culebrita.py
+++ culebrita.py
@@ -19,6 +19,7 @@ class Juego:
         self.jugando = True
         self.cantidad_comida = 0
         self.hay_cabeza=False
+        self.snake = []
     def nuevo(self):
         self.comidas = pygame.sprite.Group()
         self.cabezas = pygame.sprite.Group()
@@ -36,18 +37,23 @@ class Juego:
 
                 if event.key == pygame.K_UP:
                     for elemento in self.cabezas:
-                        elemento.moverArriba()                    
+                        if elemento.dir != 8:
+                            elemento.moverArriba()                    
                 if event.key == pygame.K_DOWN:
                     for elemento in self.cabezas:
-                        elemento.moverAbajo()
+                        if elemento.dir != 2:
+                            elemento.moverAbajo()
                 if event.key == pygame.K_LEFT:
                     for elemento in self.cabezas:
-                        elemento.moverIzquierda()
+                        if elemento.dir != 6:
+                            elemento.moverIzquierda()
                 if event.key == pygame.K_RIGHT:
                     for elemento in self.cabezas:
-                        elemento.moverDerecha()
+                        if elemento.dir != 4:
+                            elemento.moverDerecha()
     def generarCabeza(self):
         cabeza = Cabeza(self)
+        self.snake.append(cabeza)
         self.cabezas.add(cabeza)
         self.hay_cabeza = True
     def generarComida(self):
@@ -62,13 +68,14 @@ class Juego:
         if self.cantidad_comida == 0:
             self.generarComida()
         self.cabezas.update()
-        self.colas
+        self.colas.update()
     def dibujar(self):
         
         self.pantalla.fill(NEGRO)
         self.dibujarCuadricula()
         self.cabezas.draw(self.pantalla)
         self.comidas.draw(self.pantalla)
+        self.colas.draw(self.pantalla)
         pygame.display.flip()
     def ejecutar(self):
         pass
@@ -85,14 +92,20 @@ class Comida(pygame.sprite.Sprite):
         self.rect.y = y*CASILLA
 
 class Cola(pygame.sprite.Sprite):
-    def __init__(self,juego,dir,xpre,ypre):
+    def __init__(self,juego,xpre,ypre,anterior):
         pygame.sprite.Sprite.__init__(self)
         self.juego = juego
         self.image = pygame.Surface([CASILLA,CASILLA])
         self.image.fill(AZUL)
         self.rect = self.image.get_rect()
+        self.rect.x = xpre
+        self.rect.y = ypre
+        self.anterior = anterior
+        self.prepos = []
     def update(self):
-        pass
+        self.prepos = [self.rect.x,self.rect.y]
+        self.rect.x = self.anterior.prepos[0]
+        self.rect.y = self.anterior.prepos[1]
 
 class Cabeza(pygame.sprite.Sprite):
     def __init__(self,juego):
@@ -106,6 +119,10 @@ class Cabeza(pygame.sprite.Sprite):
         self.vel_x = 0
         self.vel_y = 0
         self.dir = -1
+        self.x = self.rect.x
+        self.y = self.rect.y
+        self.prepos = []
+
     def moverArriba(self):
         if self.dir == 8:
             self.dir = 2
@@ -134,15 +151,55 @@ class Cabeza(pygame.sprite.Sprite):
             self.vel_x+=CASILLA
             self.vel_y = 0
             self.dir = 4
+    def colisionComida(self):
+        for item in self.juego.comidas:
+            if pygame.sprite.collide_rect(self,item):
+                print "colision comida"
+                item.kill()
+                del item
+                self.juego.cantidad_comida = 0
+                cola = Cola(self.juego,self.juego.snake[-1].prepos[0],self.juego.snake[-1].prepos[1],self.juego.snake[-1])
+                self.juego.colas.add(cola)
+                self.juego.snake.append(cola)
     def update(self):
+        self.colisionComida()
         if ((self.dir == 6)and(self.rect.x<(ANCHO-CASILLA))):
+            self.prepos = [self.rect.x,self.rect.y]
             self.rect.x += self.vel_x
         if ((self.dir == 4)and(self.rect.x>0)):
+            self.prepos = [self.rect.x,self.rect.y]
             self.rect.x -= self.vel_x
         if ((self.dir == 8)and(self.rect.y<ALTO-CASILLA)):
+            self.prepos = [self.rect.x,self.rect.y]
             self.rect.y += self.vel_y
         if ((self.dir == 2)and(self.rect.y>0)):
+            self.prepos = [self.rect.x,self.rect.y]
             self.rect.y -= self.vel_y
+        #self.colisionComida()
+        if self.dir == 2:
+            self.y-=CASILLA
+            if self.y<0:
+                print "colision arriba"
+                pygame.quit()
+                sys.exit()
+        if self.dir == 8:
+            self.y+=CASILLA
+            if self.y>=ALTO:
+                print "colision abajo"
+                pygame.quit()
+                sys.exit()
+        if self.dir == 6:
+            self.x+=CASILLA
+            if self.x>=ANCHO:
+                print "colision derecha"
+                pygame.quit()
+                sys.exit()
+        if self.dir == 4:
+            self.x-=CASILLA
+            if self.x<0:
+                print "colision izquierda"
+                pygame.quit()
+                sys.exit()
    
     
 if __name__ == "__main__":
@@ -158,7 +215,7 @@ if __name__ == "__main__":
         #juego.comidas.draw(juego.pantalla)
         juego.dibujar()
         juego.eventos()
-        reloj.tick(10)
+        reloj.tick(5)
     pygame.quit()
     sys.exit()
     

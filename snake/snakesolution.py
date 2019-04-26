7831605d99a57e5ffab60af9bbde5cfb511913e7
Luis Martínez <lufmartinezmu@gmail.com>
2019-04-25 22:44:12 -0500

    Mapa de snake con solución
    En este programa se soluciona un problema de snake, dado un mapa inicial (estado inicial) se lleva al estado tal que se capture la comida utilizando la librería simpleai y el algoritmo A*

---
+++ snake/snakesolution.py
@@ -0,0 +1,189 @@
+from __future__ import print_function
+import sys
+from simpleai.search import astar, SearchProblem
+from simpleai.search.viewers import WebViewer
+
+GOAL = '''x-x-x-x-x-x-x-x-x-x
+x-e-e-e-e-e-e-e-e-x
+x-e-e-e-e-e-e-x-e-x
+x-e-e-e-e-e-e-x-e-x
+x-e-e-e-e-e-e-x-e-x
+x-e-e-e-e-x-x-x-e-x
+x-e-x-e-e-x-e-e-e-x
+x-x-x-e-e-x-e-e-e-x
+x-e-x-e-e-e-e-e-e-x
+x-e-x-e-e-e-e-e-e-x
+x-e-x-e-x-e-x-x-x-x
+x-e-x-e-x-e-e-x-e-x
+x-e-e-e-x-e-e-x-e-x
+x-e-e-x-x-x-e-x-e-x
+x-e-e-e-e-x-e-x-e-x
+x-e-e-e-e-x-e-x-e-x
+x-x-x-x-e-x-e-x-e-x
+x-e-e-e-e-e-e-x-e-x
+x-e-e-e-e-e-e-e-e-x
+x-e-e-e-e-e-e-e-e-x
+x-e-e-x-x-x-e-e-e-x
+x-e-e-e-e-x-e-e-e-x
+x-e-e-e-e-x-e-e-e-x
+x-e-e-e-e-x-e-e-e-x
+x-e-x-x-e-x-e-e-e-x
+x-e-e-x-e-x-x-x-x-x
+x-e-c-x-e-e-e-e-e-x
+x-e-e-x-e-e-e-e-e-x
+x-e-e-x-e-e-e-e-e-x
+x-x-x-x-x-x-x-x-x-x
+'''
+
+INITIAL = '''x-x-x-x-x-x-x-x-x-x
+x-e-e-e-e-e-e-e-e-x
+x-e-e-e-e-e-e-x-e-x
+x-e-e-e-e-e-c-x-e-x
+x-e-e-e-e-e-e-x-e-x
+x-e-e-e-e-x-x-x-e-x
+x-e-x-e-e-x-e-e-e-x
+x-x-x-e-e-x-e-e-e-x
+x-e-x-e-e-e-e-e-e-x
+x-e-x-e-e-e-e-e-e-x
+x-e-x-e-x-e-x-x-x-x
+x-e-x-e-x-e-e-x-e-x
+x-e-e-e-x-e-e-x-e-x
+x-e-e-x-x-x-e-x-e-x
+x-e-e-e-e-x-e-x-e-x
+x-e-e-e-e-x-e-x-e-x
+x-x-x-x-e-x-e-x-e-x
+x-e-e-e-e-e-e-x-e-x
+x-e-e-e-e-e-e-e-e-x
+x-e-e-e-e-e-e-e-e-x
+x-e-e-x-x-x-e-e-e-x
+x-e-e-e-e-x-e-e-e-x
+x-e-e-e-e-x-e-e-e-x
+x-e-e-e-e-x-e-e-e-x
+x-e-x-x-e-x-e-e-e-x
+x-e-e-x-e-x-x-x-x-x
+x-e-e-x-e-e-e-e-e-x
+x-e-e-x-e-e-e-e-e-x
+x-e-e-x-e-e-e-e-e-x
+x-x-x-x-x-x-x-x-x-x
+'''
+
+def list_to_string(list_):
+    return '\n'.join(['-'.join(row) for row in list_])
+
+
+def string_to_list(string_):
+    return [row.split('-') for row in string_.split('\n')]
+
+
+def find_location(rows, element_to_find):
+    '''Find the location of a piece in the puzzle.
+       Returns a tuple: row, column'''
+    for ir, row in enumerate(rows):
+        for ic, element in enumerate(row):
+            if element == element_to_find:
+                return ir, ic
+
+
+# we create a cache for the goal position of each piece, so we don't have to
+# recalculate them every time
+goal_positions = {}
+rows_goal = string_to_list(GOAL)
+for food in 'c':
+    goal_positions['c'] = find_location(rows_goal, food)
+
+
+
+
+class SnakeProblem(SearchProblem):
+    def actions(self, state):
+        '''Returns a list of the pieces we can move to the empty space.'''
+        rows = string_to_list(state)
+        row_c, col_c = find_location(rows, 'c')
+
+        actions = []
+        if rows[row_c-1][col_c] == 'f':
+            actions.append("UP")
+            rows[row_c-1][col_c]='e'
+        if rows[row_c+1][col_c] == 'f':
+            actions.append("DOWN")
+            rows[row_c+1][col_c] = 'e'
+        if rows[row_c][col_c-1] == 'f':
+            actions.append("LEFT")
+            rows[row_c][col_c-1] = 'e'
+        if rows[row_c][col_c+1] == 'f':
+            actions.append("RIGHT")
+            rows[row_c][col_c+1] = 'e'
+        else:
+            if rows[row_c-1][col_c] == 'e':
+                actions.append("UP")
+            if rows[row_c+1][col_c] == 'e':
+                actions.append("DOWN")
+            if rows[row_c][col_c-1] == 'e':
+                actions.append("LEFT")
+            if rows[row_c][col_c+1] == 'e':
+                actions.append("RIGHT")
+        #print(actions)
+
+        return actions
+
+
+    def result(self, state, action):
+        '''Return the resulting state after moving a piece to the empty space.
+           (the "action" parameter contains the piece to move)
+        '''
+        rows = string_to_list(state)
+        row_c, col_c = find_location(rows, 'c')
+        #print(row_c,col_c)
+        if action == "UP":
+            rows[row_c][col_c], rows[row_c-1][col_c] = rows[row_c-1][col_c], rows[row_c][col_c]
+        if action == "DOWN":
+            rows[row_c][col_c], rows[row_c+1][col_c] = rows[row_c+1][col_c], rows[row_c][col_c]
+        if action == "RIGHT":
+            rows[row_c][col_c], rows[row_c][col_c+1] = rows[row_c][col_c+1], rows[row_c][col_c]
+        if action == "LEFT":
+            rows[row_c][col_c], rows[row_c][col_c-1] = rows[row_c][col_c-1], rows[row_c][col_c]
+        #if (row_c == 26 and col_c==2):
+            #print(list_to_string(rows))
+            
+        #print (action)
+        #print(rows)
+        
+        
+
+        return list_to_string(rows)
+
+
+    def is_goal(self, state):
+        '''Returns true if a state is the goal state.'''
+        return state == GOAL
+
+    def cost(self, state1, action, state2):
+        '''Returns the cost of performing an action. No useful on this problem, i
+           but needed.
+        '''
+        return 1
+
+    def heuristic(self, state):
+        '''Returns an *estimation* of the distance from a state to the goal.
+           We are using the manhattan distance.
+        '''
+        rows = string_to_list(state)
+
+        distance = 0
+
+        row_c, col_c = find_location(rows, 'c')
+        row_c_goal, col_c_goal = goal_positions['c']
+
+        distance += abs(row_c - row_c_goal) + abs(col_c - col_c_goal)
+        #print(distance)
+        #sys.exit()
+        return distance
+
+
+result = astar(SnakeProblem(INITIAL),graph_search=True)
+# if you want to use the visual debugger, use this instead:
+# result = astar(EigthPuzzleProblem(INITIAL), viewer=WebViewer())
+
+for action, state in result.path():
+    print('Move ', action)
+print(state)
